01265292791efd1b2632663cb855d3b06b6e8259
clau9156 <clau9156@stud.kea.dk>
2021-11-24 16:24:28 +0100

    feat (app) created navigation with logo and links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,14 @@
 export default function navBar() {
   return(
     <div className="navBar">
+        <a className="home" href="/home"><img className="logo" src="./image/logoblue2.png" alt="logo"></img></a>
+        <div className="navB">
+          <a className="navButton" href="/about">About</a>
+          <a className="navButton" href="/contact">Contact</a>
+          <a className="navButton"href="/qr">Get Your QR</a>
+        </div>
       </div>
+
+      
   );
 }
