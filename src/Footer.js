117576050d5b2a29a3061647b123d0a2c5a7c17b
marwane <marw0619@stud.kea.dk>
2021-11-24 18:36:23 +0100

    feat(app)made the footer not linked with logo

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,59 @@
 export default function Footer() {
   return(
-    <div className="footer"></div>
+    <div className="footer">
+      
+      <div className="fstFContainer">
+        <div>
+          <img alt="" className="footerMonta" src="./image/karmapink.png"></img>
+          <select>
+            <option>Dansk</option>
+            <option>English</option>
+            <option>French</option>
+          </select>
+        
+
+        
+        
+          <p>Downlaod:</p>
+          <div>
+            <img></img>
+            <img></img>
+          </div>
+        </div>
+      </div>
+      <div className="scdFContainer">
+        <div>
+        <a href="/" >Home</a>
+        <span></span>
+        <a href="/" >About</a>
+        <span></span>
+        <a href="/" >Order the QR</a>
+        <span></span>
+        <a href="/" >Contact</a>
+        <span></span>
+        </div>
+        
+        <p>2021@ Copyright Karma</p>
+      </div>
+      <div className="trdFContainer">
+        <div>
+          <p>Find Us On:</p>
+          <a href="/" >Facebook</a>
+          <a href="/" >Instagram</a>
+          <a href="/" >LinkedIn</a>
+        </div>
+        <div>
+        <p>Contact:</p>
+          <p>Dalslandgade 6th,<br></br>
+          2300 <br></br>
+          Kobenhavn S</p>
+          <p className="phoneNum">+45 96 09 54 68</p>
+        </div>
+
+        
+  
+      </div>
+    
+    </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 export default function navBar() {
   return(
     <div className="navBar">
-        <a className="home" href="/home"><img className="logo" src="./image/logoblue2.png" alt="logo"></img></a>
+        <a className="homeButton" href="/home"><img className="logo" src="./image/logoblue2.png" alt="logo"></img></a>
         <div className="navB">
           <a className="navButton" href="/about">About</a>
           <a className="navButton" href="/contact">Contact</a>
