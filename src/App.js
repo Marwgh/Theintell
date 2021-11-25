8078d7706f122c53cf5cfc39d73247b5bfe3fbb4
clau9156 <clau9156@stud.kea.dk>
2021-11-25 16:55:30 +0100

    feature(app) adding content in carjourney

--- src/App.js
+++ src/App.js
@@ -24,7 +24,24 @@ function App() {
           
         </div>
       </div>
-      <div className="homeCarjourney"></div>
+      <div className="homeCarjourney">
+        <div className="homeCarjourneyText">
+          <h1>WE MAKE CHARGING EASIER THAN EVER</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div className="homeCarjourneyPath">
+        {/* <svg height="500vw" width="100%"><path d="M 0 500 L 500 500 L 850 400 L 1200 460 L 1600 350" fill="white"stroke="black"></path></svg> */}
+        <svg xmlns="http://www.w3.org/2000/svg" width="1947.492" height="214.311" viewBox="0 0 1947.492 214.311">
+          <g id="lines" transform="translate(0 -1757.461)">
+            <path id="Line_12" data-name="Line 12" d="M671,5H0V-5H671Z" transform="translate(0 1951.233)" fill="#00044f"/>
+            <path id="Line_19" data-name="Line 19" d="M1.948,191.539l-3.9-9.21,442-186.934,3.9,9.21Z" transform="translate(659 1780.233)" fill="#00044f"/>
+            <path id="Line_20" data-name="Line 20" d="M413.665,119.818l-415-115,2.67-9.637,415,115Z" transform="translate(1077.865 1775.601)" fill="#00044f"/>
+            <path id="Line_21" data-name="Line 21" d="M1.492,140.772l-2.984-9.544,435-136,2.984,9.544Z" transform="translate(1511 1762.233)" fill="#00044f"/>
+          </g>
+        </svg>
+        </div>
+      </div>
       <div className="homeKIP"></div>
       <div className="homeSection"></div>
       <Footer/>
