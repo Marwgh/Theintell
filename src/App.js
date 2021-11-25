9b297309ef4fe7053062fbcffdcb4abfe1062952
marwane <marw0619@stud.kea.dk>
2021-11-25 15:49:50 +0100

    feat(App)Making the hreo section with images

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <div className="homeTitle"></div>
-      <div className="homeStatistics"></div>
+      <div className="homeTitle">
+        <div className="homeText">
+          <h1>WE MAKE CHARGING EASIER THAN EVER</h1>
+          <p>Learn about Karma and how you can help your fellow electric cars owners to charge their car.
+            Come and discover the amazing opportunity's and get your own Qr code! 
+          </p>
+          <a className="getQrBtnDescription" href="./">Get your code</a>
+        </div>
+        <div className="buildings">
+          <img src="./svg/bUILDINGS.svg" alt="buildings"></img>
+        </div>
+        <div className="phoneHome">
+          <img src="./svg/phoneHome.svg" alt="phone on qr code page"></img>
+        </div>
+        <div className="charginCarHome">
+        <img src="./svg/backcar.svg" alt="charging car"></img>
+          
+        </div>
+      </div>
+      <div className="homeCarjourney"></div>
       <div className="homeKIP"></div>
       <div className="homeSection"></div>
       <Footer/>
