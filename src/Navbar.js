cec19b0e8c21ba756da276d2202b62c20fce5bd5
clau9156 <clau9156@stud.kea.dk>
2021-11-25 14:32:46 +0100

    feature (app) fixed the header

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,9 @@ export default function navBar() {
         <a className="homeButton" href="/home"><img className="logo" src="./image/logoblue2.png" alt="logo"></img></a>
         <div className="navB">
           <a className="navButton" href="/about">About</a>
+          <a className="navButton" href="/howitworks">How It Works</a>
           <a className="navButton" href="/contact">Contact</a>
-          <a className="navButton"href="/qr">Get Your QR</a>
+          <a className="navButton qr" href="/qr">Get Your QR</a>
         </div>
       </div>
 
