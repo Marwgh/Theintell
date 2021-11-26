69e0531d49f91261a31915f29edc6f38680bfe3e
clau9156 <clau9156@stud.kea.dk>
2021-11-26 17:46:03 +0100

    feat(home) created car journey section, fixed phone display below, started working on the sections below

--- src/Home.js
+++ src/Home.js
@@ -28,25 +28,45 @@ export default function Home(props) {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
           </p>
         </div>
-        <div className="homeCarjourneyPath">
-        
+        <div className="homeCarjourneyTL">
+          <img className="car" src="./svg/car.svg" alt="car"></img>
+          <img className="charger" src="./svg/charger.svg" alt="car"></img>
+          <img className="phone" src="./svg/phone.svg" alt="car"></img>
+          <img className="reward" src="./svg/reward.svg" alt="car"></img>
+          <div className="dot1"></div>
+          <div className="dot2"></div>
+          <div className="dot3"></div>
+          <img className="timeline" src="./svg/timeline.svg" alt="timeline"></img>
         </div>
       </div>
       <div className="homeHAW">
         <div className="hawText">
-          <h1>How are we?</h1>
+          <h1>Who are we?</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
           </p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
           </p>
           <div className="getQrBtnHAW" onClick={ () => props.changePage("Howto")}>Get your code</div>
         </div>
-        <div>  <img src="/svg/hawPhones.svg" alt="app show case "></img> </div>
+        <div>  <img src="/svg/hawPhones.svg" alt="app show case"></img> </div>
 
       </div>
-      <div className="homeSection"></div>
-
-
+      <div className="homeTrust">
+        <h1>Trusted by:</h1>
+      </div>
+      <div className="homeSection">
+        <div> <img src="" alt="app show case"></img> </div>
+        <div className="homeSectionText">
+        <h1>Title here</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+          {/* <div className="getQrBtnSection" onClick={ () => props.changePage("Howto")}>Get your code</div> */}
+        </div>
+      </div>
+      <div className="homeHIW"></div>
+      <div className="homeContact"></div>
     </div>
   );
 }
