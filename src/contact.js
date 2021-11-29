02165537c8c5a951b05f8724a484a61b9955af1f
clau9156 <clau9156@stud.kea.dk>
2021-11-29 19:05:23 +0100

    feat(howto) finished all layouts except the bottom section image on different computer & feat(contact) started positioning text

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,31 @@
-
+import './Contact.css';
 export default function Contact() {
   return (
     <div className="contact">
+      <div className="contactIntro">
+        <div className="introText">
+          <h1>How can we help you?</h1>
+          <p>Learn about Karma and how you can help your fellow electric cars owners to charge their car. Come and discover the amazing opportunity's and get your own Qr code! </p>
+        </div>
+      </div>
 
+      <div className="FAQ">
+        <h1>Frequently Asked Questions</h1>
+      </div>
+
+      <div className="Questions">
+
+      </div>
+
+      <div className="contactForm">
+        <div className="formTitle">
+          <h1>We couldn't answer your questions?</h1>
+          <p>If you have any inquiries about Karma Charging, or want to discuss a solution for your business, write to us here.</p>
+        </div>
+        <div className="form">
+
+        </div>
+      </div>
 
     </div>
   );
--- src/howTo.js
+++ src/howTo.js
@@ -1,41 +1,75 @@
+import './HowTo.css';
 export default function Howto(props) {
   return (
-    <div className="Howto">
-      <div className="">
-        <div className="">
+    <div className="howTo">
+      <div className="hiwIntro">
+        <div className="introText">
           <h1>How It Works</h1>
           <p>If you have any inquiries about Karma Charging, or want to discuss a solution for your business, write to us here.</p>
         </div>
+        <div className="car">
+          <img src="./svg/car.svg" alt="car"></img>
+        </div>
+        <div className="buildings">
+          <img src="./svg/darkbuildings.svg" alt="buildings"></img>
+        </div>
+        <div className="phoneDisplay">
+          <img src="./svg/hawPhones.svg" alt="2 phones displayed"></img>
+        </div>
       </div>
 
-      <div className="">
-        <h1>How It Works</h1>
-        <p>If you have any inquiries about Karma Charging, or want to discuss a solution for your business, write to us here.</p>
-        <div className="" onClick={ () => props.changePage("Order")}>Get your code</div>
+      <div className="howitworks">
+        <div className="howitworksText">
+          <h1>How It Works</h1>
+          <p>If you have any inquiries about Karma Charging, or want to discuss a solution for your business, write to us here.</p>
+          <div className="orderButton" onClick={ () => props.changePage("Order")}>Get your code</div>
+        </div>
       </div>
 
-      <div className="">
-        <div className="row1">
-          <div className="">
+      <div className="explain">
+        <div className="charger">
+          <div className="chargerText">
             <h1>Title here</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
           </div>
+          <div className="chargerImg">
+            <img src="./svg/charger.svg" alt="charger"></img>
+          </div>
+          <div className="qrcardImg">
+            <img src="./svg/qrcard.svg" alt="qr card"></img>
+          </div>
         </div>
-        <div className="row2">
-          <div className="">
+        <span></span>
+        <div className="phone">
+          <div className="phoneText">
             <h1>Title here</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
           </div>
+          <div className="plugImg">
+            <img src="./svg/plug.svg" alt="plug"></img>
+          </div>
+          <div className="phoneImg">
+            <img src="./svg/phone.svg" alt="phone"></img>
+          </div>
         </div>
-        <div className="row3">
-          <div className="">
+        <span></span>
+        <div className="reward">
+          <div className="rewardText">
             <h1>Title here</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          </div>
+          <div className="carImg">
+            <img src="./svg/car.svg" alt="car"></img>
+          </div>
+          <div className="rewardImg">
+            <img src="./svg/reward.svg" alt="reward"></img>
           </div>
         </div>
       </div>
       <div className="pinkrow"></div>
-      <div className="imgrow"></div>
+      <div className="imgrow">
+        <img src="./image/" alt="backgroundimg"></img>
+      </div>
     </div>
   );
 }
