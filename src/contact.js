de6cac84c9468f554fb23edaf1f39eabe6ceeac9
clau9156 <clau9156@stud.kea.dk>
2021-11-30 18:33:43 +0100

    feat(home) fixed 1 small thing, feat(contact) top section created, feat(howto) fixed, created two more css for other pages (not connected yet

--- src/contact.js
+++ src/contact.js
@@ -3,9 +3,34 @@ export default function Contact() {
   return (
     <div className="contact">
       <div className="contactIntro">
-        <div className="introText">
+        <div className="contactIntroText">
           <h1>How can we help you?</h1>
-          <p>Learn about Karma and how you can help your fellow electric cars owners to charge their car. Come and discover the amazing opportunity's and get your own Qr code! </p>
+          <p>Learn about Karma and how you can help your fellow electric cars owners to charge their car. Come and discover the amazing opportunity's and get your own QR code! </p>
+        </div>
+        <div className="customers">
+          <img src="./svg/customers.svg" alt="customers"></img>
+        </div>
+        <div className="envelopeLeft">
+          <img src="./svg/EnvelopePink2.svg" alt="envelope"></img>
+        </div>
+        <div className="envelopeRight">
+          <img src="./svg/EnvelopePink1.svg" alt="envelope"></img>
+        </div>
+        <div className="textboxLeft">
+          <p>How do I make an account?</p>
+        </div>
+        <div className="textboxRight">
+          <p>How do I use my Karma points?</p>
+        </div>
+        <div className="lineLeft">
+          <svg> 
+            <path stroke-dasharray="16, 16" d="M5 20 l150 0" fill="none" stroke="#3f51b5" stroke-width="2"/>
+          </svg>
+        </div>
+        <div className="lineRight">
+          <svg> 
+            <path stroke-dasharray="16, 16" d="M5 20 l120 0" fill="none" stroke="#3f51b5" stroke-width="2"/>
+          </svg>
         </div>
       </div>
 
