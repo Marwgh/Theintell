c6bbfbb40a40bb2b9c7c5b05c02712dcb1b4af5a
clau9156 <clau9156@stud.kea.dk>
2021-12-09 15:31:30 +0100

    feat(home) almost done missing some images

--- src/Home.js
+++ src/Home.js
@@ -11,17 +11,15 @@ export default function Home(props) {
           </p>
           <div className="getQrBtnDescription" onClick={ () => props.changePage("Howto")}>Get your code</div>
         </div>
-        <div className="buildings">
-          <img src="./svg/bUILDINGS.svg" alt="buildings"></img>
+        <div className="homeBackgroundImg">
+          <img src="./image/homecarchargeQR.jpg" alt="Car charger with QR"></img>
         </div>
-        <div className="phoneHome">
-          <img src="./svg/phoneHome.svg" alt="phone on qr code page"></img>
+        <div className="standingHome">
+          <img src="./svg/standingblue.svg" alt="Blue person standing"></img>
         </div>
-        <div className="charginCarHome">
-        <img src="./svg/backcar.svg" alt="charging car"></img>
-          
-        </div>
+
       </div>
+
       <div className="homeCarjourney">
         <div className="homeCarjourneyText">
           <h1>A JOURNEY BASED ON KARMA</h1>
@@ -42,37 +40,81 @@ export default function Home(props) {
       <div className="homeHAW">
         <div className="hawText">
           <h1>Who are we?</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          <p>Karma charging was built  by EV enthusiasts for  EV users, to ensure they will never have to wait for a charging spot again.
           </p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          <p>As a part of the Month Group, we are familiar to the community needs and problems and we embarked on a  journey to ease the life of EV owners.
           </p>
-          <div className="getQrBtnHAW" onClick={ () => props.changePage("Order")}>Get your code</div>
+          <div className="getQrBtnHAW" onClick={ () => props.changePage("About")}>Read more about us</div>
         </div>
         <div>  <img src="/svg/hawPhones.svg" alt="app show case"></img> </div>
 
       </div>
+
+      <div className="homeData">
+        <div className="homeDataHeader">
+          <h1>Don’t trust us?</h1>
+          <h1>Trust the <span>community</span></h1>
+          <p>Karma charging was built  by EV enthusiasts for  EV users, to ensure they will never have to wait for a charging spot again. </p>
+        </div>
+        <div className="homeDataInfo">
+          <div className="column1">
+            <img src="/svg/" alt="chart"></img>
+            <div className="text1">
+              <h1>EV owner's</h1>
+              <p>We asked if you  would move your car even when not fully charged to allow one on low battery instead?</p>
+            </div>
+          </div>
+          <div className="column2">
+            <img src="/svg/" alt="chart"></img>
+            <div className="text2">
+              <h1>Lorem Ipsum</h1>
+              <p>Lorem Ipsum</p>
+            </div>
+          </div>
+          <div className="column3">
+            <img src="/svg/" alt="chart"></img>
+            <div className="text3">
+              <h1>Lorem Ipsum</h1>
+              <p>Karma charging was built by EV enthusiasts for EV users.</p>
+            </div>
+          </div>
+          <div className="column4">
+            <img src="/svg/" alt="chart"></img>
+            <div className="text4">
+              <h1>Lorem Ipsum</h1>
+              <p>Lorem Ipsum</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="homeHIW">
+        <div> <img src="/svg/carscanning.svg" alt="app show case"></img> </div>
+        <div className="homeHIWText">
+        <h1>How it works</h1>
+          <p>We explain to you where can you use Karma, what do you need  and the journey you will be taking.
+          </p>
+          <p>As well you can find some explanations on what is the QR code for and what king of sweet deals you can get by using it. 
+          </p>
+          <div className="homeHIWButton" onClick={ () => props.changePage("HowTo")}>Read more</div>
+        </div>
+      </div>
+
+      <div className="homeGetCode">
+        <div className="homeGetCodeinfo">
+          <h1>Get your code</h1>
+          <img src="/svg/3phonedisplay.svg" alt="apps displays"></img>
+          <p>Becoming a part of the Karma community is a simple process that starts with a QR code.</p>
+          <div className="GetCodeButton" onClick={ () => props.changePage("Howto")}>Get your code now</div>
+        </div>
+      </div>
+
       <div className="homeTrust">
         <h1>Trusted by:</h1>
+        <img className="arrowLeft" src="/svg/arrowleft.svg" alt="arrow left"></img>
+        <img className="arrowRight" src="/svg/arrowRight.svg" alt="arrow Right"></img>
       </div>
-      <div className="homeSection">
-        <div> <img src="/svg/phonedisplay.svg" alt="app show case"></img> </div>
-        <div className="homeSectionText">
-        <h1>Title here</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-          </p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
-          </p>
-          <div className="homeSectionQRButton" onClick={ () => props.changePage("Order")}>Get your code</div>
-        </div>
-      </div>
-      <div className="homeHIW">
-        <div className="homeHIWinfo">
-          <h1>How It Works</h1>
-          <img src="/svg/3phonedisplay.svg" alt="apps displays"></img>
-          <p>If you have any inquiries about Karma Charging, or want to discuss a solution for your business, write to us here. </p>
-          <div className="HIWButton" onClick={ () => props.changePage("Howto")}>How to use Karma</div>
-        </div>
-      </div>
+
       <div className="homeContact">
         <div className="contactText">
           <h1>Contact Us</h1>
@@ -80,6 +122,9 @@ export default function Home(props) {
           <div className="contactButton" onClick={ () => props.changePage("Contact")}>Contact</div>
         </div>
         <img src="/svg/EnvelopeBlue.svg" alt="blue envelope"></img>
+        <div className="contactBGimg">
+          <img src="/image/homecharge.jpg" alt="charge with blur"></img>
+        </div>
       </div>
     </div>
   );
